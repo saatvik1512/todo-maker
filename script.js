f30710479906a8248aa45a48d4559f8cd73b394e
Saatvik <saatvik.jbp@gmail.com>
2024-01-17 10:44:39 +0530

    console version is ready

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const inbox = []
 
-function addTodoToList(){
-    const createTodo = () => {
-        const title = prompt('Choose title');
+function createTodo()  {
+    const title = prompt('Choose title');
         const description = prompt('note description');
         const dueDate = prompt('choose a date');
         const priority = prompt('choose priority');
@@ -22,23 +21,39 @@ function addTodoToList(){
                 console.log(`priority has been changed to ${opinion}`)
             }
         }
-    }
+}
+
+function addTodoToList()  {
     const newTodo = createTodo();
-    if (newTodo.title != '' && checkPriority(newTodo.priority)) inbox.push(newTodo)
+    const array_Name = prompt('Which array to add');
+    if (newTodo.title != '' && checkPriority(newTodo.priority)) setItemToArray(newTodo, array_Name)
     console.table(inbox)
 }
 
-
-function checkPriority(word){
+function checkPriority(word)  {
     const arr = ['low', 'high', 'normal'];
     if (arr.includes(word.toLowerCase())) return true;
     return false;
 }
 
-
-function createProject(arrayName){
+function createProject(arrayName)  {
     //it will create a new project or a new array
-    const newArr = new Array()
-    arrayName = newArr;
-    
+    const newArr = [];
+    localStorage.setItem(arrayName, JSON.stringify(newArr))
+}
+
+function setItemToArray(newTodo, array_Name)  {
+    if (localStorage.getItem(array_Name) == null) return
+    const returnedArray = JSON.parse(localStorage.getItem(array_Name))
+    returnedArray.push(newTodo);
+    localStorage.setItem(array_Name, JSON.stringify(returnedArray))
+}
+
+function removeItemFromArray(){
+    const arrayName = prompt('Name of the Array');
+    const itemTitle = prompt('item of title');
+    const derivedArray = JSON.parse(localStorage.getItem(arrayName));
+    const index = derivedArray.findIndex(item => item.title === itemTitle);
+    if (index !== -1) derivedArray.splice(index, 1);
+    localStorage.setItem(arrayName, JSON.stringify(derivedArray))
 }
